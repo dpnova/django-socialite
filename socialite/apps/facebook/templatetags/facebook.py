9d027b9a654e2d87174eb0ffaadd2ac5c4aa2f7f
David Novakovic <davidnovakovic@gmail.com>
2011-09-11 13:13:08 +1000

    Add template tags for getting avatar for facebook.

---
+++ socialite/apps/facebook/templatetags/facebook.py
@@ -0,0 +1,11 @@
+from django import template
+from socialite.apps.linkedin.helper import get_avatar
+
+register = template.Library()
+
+
+@register.simple_tag()
+def facebook_avatar(size, user, user_id="me"):
+    return get_avatar(access_token=user.facebookservices.all()[0].access_token, 
+                      user_id=user_id, 
+                      size=size)
